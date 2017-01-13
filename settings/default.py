904d9a0e5b546703d8b9a4b7c35f042bf64631f1
Wouter Kobes <git@wouterkobes.nl>
2017-01-13 15:05:33 +0100

    Adds multiverse app, initial app structure

--- settings/default.py
+++ settings/default.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'apps.general',
+    'apps.multiverse',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -60,7 +61,7 @@ ROOT_URLCONF = 'urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
