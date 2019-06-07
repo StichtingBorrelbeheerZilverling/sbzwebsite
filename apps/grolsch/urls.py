b7e7d32130950a0673b84e6ae995ea7824f628e0
Pieter <info@pieterbos.me>
2019-06-07 21:35:08 +0200

    fixes #6: add delete button for Grolsch products

--- apps/grolsch/urls.py
+++ apps/grolsch/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     url('^products$', views.ProductList.as_view(), name='products'),
     url('^product/create$', views.ProductCreate.as_view(), name='product_create'),
+    url('^product/(?P<pk>[0-9]+)/delete$', views.ProductDelete.as_view(), name='product_delete'),
 
     url('^price_changes$', views.PriceChangeList.as_view(), name='price_changes'),
     url('^price_change/(?P<pk>[0-9]+)$', views.PriceChangeDetail.as_view(), name='price_change_detail'),
