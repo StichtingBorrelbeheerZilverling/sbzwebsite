538564150a1c8eb5dda6a6c43cecdcd7d607c630
Bram van Dartel <bram@vandartel.eu>
2021-06-30 13:51:02 +0200

    Update base URL multivers

--- apps/multivers/tools.py
+++ apps/multivers/tools.py
@@ -131,7 +131,7 @@ class MultiversOrder:
 
 
 class Multivers:
-    BASE_URL = "https://api.online.unit4.nl/V19/"
+    BASE_URL = "https://api.online.unit4.nl/V22/"
     EXPIRE_MARGIN = 5*60 # seconds
 
     def __init__(self, request, client_id=None, client_secret=None):
