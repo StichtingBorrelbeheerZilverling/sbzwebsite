cee56be920dda7704f823049a943fa20547b084f
Dilan van Oosten <537670@student.saxion.nl>
2024-04-22 17:00:31 +0200

    #54 Trying to set up local development environment

--- settings/default.py
+++ settings/default.py
@@ -4,7 +4,7 @@ Django settings for sbzwebsite project.
 
 import os
 
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
